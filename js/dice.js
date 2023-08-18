8a1112b2b51b1f9d37a6f29b37fcfd9042bb5d75
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-18 14:36:10 +0200

    result in console

--- js/dice.js
+++ js/dice.js
@@ -7,3 +7,15 @@ Stampare (in console)
 // Randomizzo i numeri
 const playerNumber = Math.floor(Math.random() * 6 ) + 1;
 const cpuNumber = Math.floor(Math.random() * 6 ) + 1;
+
+// Variabile result
+let result = 'Pareggio';
+
+// Controllo chi ha vinto
+if(playerNumber > cpuNumber) {
+    result = 'Ha vinto il giocatore';
+} else if(playerNumber < cpuNumber) {
+    result = 'Ha vinto la CPU';
+}
+
+console.log(result)
